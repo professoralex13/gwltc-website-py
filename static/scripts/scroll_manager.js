ec6382af8bb163ae6ec2918d777618ccb1e5ba23
Alex Cutforth <alex@densem.net>
2022-02-26 12:24:25 +1300

    add logos

--- static/scripts/scroll_manager.js
+++ static/scripts/scroll_manager.js
@@ -1,4 +1,6 @@
 const links = Array.from(document.getElementsByClassName('header-link'));
+const [nav] = document.getElementsByTagName('nav');
+const navLogo = document.getElementById('nav-logo');
 let targets = [];
 
 links.forEach((link, i) => {
@@ -14,10 +16,19 @@ links.forEach((link, i) => {
 });
 
 window.addEventListener('scroll', () => {
+    if(window.scrollY >= window.innerWidth * 0.479) {
+        nav.classList.add('nav-scrolled');
+        navLogo.style.opacity = '1';
+    } else {
+        nav.classList.remove('nav-scrolled');
+        navLogo.classList.remove('nav-logo-active');
+        navLogo.style.opacity = '';
+    }
+
     for (const link of links) {
         link.classList.remove('active');
     }
-    let index = targets.length - [...targets].reverse().findIndex((section) => window.scrollY >= section.offsetTop - window.innerWidth * 0.041);
+    let index = targets.length - [...targets].reverse().findIndex((section) => window.scrollY >= section.offsetTop - window.innerWidth * 0.039);
     if(index === targets.length + 1) index = 0;
     links[index].classList.add('active');
 });
