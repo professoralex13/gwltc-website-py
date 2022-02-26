7800f37cf643c76e5254dac9535563c16d1ac2d6
Alex Cutforth <alex@densem.net>
2022-02-26 15:33:00 +1300

    feat: account dropdown

---
+++ static/scripts/account_dropdown.js
@@ -0,0 +1,20 @@
+const button = document.getElementById('account-button');
+const dropdown = document.getElementById('account-dropdown');
+
+let open = false;
+
+button.addEventListener('click', (event) => {
+   console.log('clicked');
+   open = !open;
+   dropdown.style.opacity = open ? '1' : '0';
+   dropdown.style.pointerEvents = 'auto';
+   event.preventDefault();
+});
+
+document.addEventListener('click', (event) => {
+   if(event.target !== dropdown && event.target !== button) {
+      open = false;
+      dropdown.style.opacity = '0';
+      dropdown.style.pointerEvents = 'none';
+   }
+});
